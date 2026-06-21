804f864690a8ccd65cf4cd82eb8fd97ebf09193e
GrabPJ <patelfarida037@gmail.com>
2026-06-21 16:27:52 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GrabPJ from "./GRABPJ.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GrabPJ />
-  </React.StrictMode>
-);
+const root = document.getElementById("root");
+
+try {
+  const GrabPJ = (await import("./GRABPJ.jsx")).default;
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <GrabPJ />
+    </React.StrictMode>
+  );
+} catch (err) {
+  root.innerHTML = `<pre style="color:red;white-space:pre-wrap;padding:20px;">${err.stack || err.message}</pre>`;
+}
