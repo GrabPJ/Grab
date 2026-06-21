e96286a10501f628b98744a26f354aaec2c54b23
GrabPJ <patelfarida037@gmail.com>
2026-06-21 16:00:35 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,2 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import GrabPJ from "./GRABPJ.jsx";
 import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GrabPJ />
+  </React.StrictMode>
+);
